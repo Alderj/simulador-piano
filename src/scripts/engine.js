1e38fc63e873e5de18a36ebce7e3a385ed2c3fe7
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-09-27 14:05:47 -0400

    tirando as letras da teclas dinamicamente c toogle

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-const volumeSlider = document.querySelector(".volume-slider input")
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck =  document.querySelector(".keys-check input");
 
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
@@ -32,6 +33,12 @@ document.addEventListener("keydown", (e)=> {
 const handleVolume = (e)=>{
     audio.volume = e.target.value;
     console.log(e.target.value);
+} 
+
+const showHideKeys = () =>{
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
 }
 
-volumeSlider.addEventListener("input", handleVolume)
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
